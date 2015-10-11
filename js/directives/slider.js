c9adb4dfc413b22b8c69864da5b2ee694481e9fe
Dan Quinn <dan@danquinndesign.com>
2015-10-11 09:37:37 -0700

    Fixes touch events

--- js/directives/slider.js
+++ js/directives/slider.js
@@ -34,36 +34,49 @@ app.directive('slider', ($window, $timeout) => {
 
       getDimensions()
 
+      let getX = (e) => {
+        if (e.originalEvent && e.originalEvent.touches) {
+          return e.originalEvent.touches[0].clientX
+        }
+        return e.clientX
+      }
+
       scope.startDrag = (e) => {
         let lastX = 0
         let dir = 1
         let startOffset = scope.offset
-        let startX = e.clientX
+        let startX = getX(e)
+
+        $wrapper.addClass('dragging')
 
         let drag = (e) => {
+          let eX = getX(e)
           let minOffset = scope.slideWidth * (scope.images.length - 1) * -1
           let maxOffset = 0
 
           scope.$apply(() => {
-            scope.offset = startOffset + (e.clientX - startX)
+            scope.offset = startOffset + (eX - startX)
 
             if (scope.offset < minOffset) { scope.offset = minOffset }
             if (scope.offset > maxOffset) { scope.offset = maxOffset }
           })
 
-          dir = (e.clientX > lastX) ? -1 : 1
+          dir = (eX > lastX) ? -1 : 1
 
-          lastX = e.clientX
+          lastX = eX
         }
 
-        $w.on('mousemove', drag)
-        $w.on('mouseup', () => {
-          scope.$apply(() => { 
+        $w.on('mousemove touchmove', drag)
+        $w.on('mouseup touchend', () => {
+          $wrapper.removeClass('dragging')
+          scope.$apply(() => {
             moveToSlide(scope.currentSlide + dir)
           })
-          $w.off('mousemove', drag)
+          $w.off('mousemove touchmove', drag)
         })
       }
+
+      $el.on('touchstart', (e) => { scope.startDrag(e) })
 
       $w.on('resize', () => {
         $timeout.cancel(resizeTimeout)
